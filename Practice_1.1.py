ecb9c32c16b46abf418bb4b483497e0bf26d4c4e
D-Shock <davidab028@gmail.com>
2025-11-28 12:10:21 -0600

    Segunda práctica independiente con int, float y str

---
+++ Practice_1.1.py
@@ -0,0 +1,6 @@
+#REMEMBER: Las variables str van entre comillas dobles o simples.
+name = "Darth Plagueis"
+x = 33
+y = 42
+
+print("Did you ever hear the tragedy of", name, "the Wise? I thought not. It's the result of", x, "and", y, ", a Sith Legend which adds up to", x + y)
