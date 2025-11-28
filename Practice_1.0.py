6d18bc0335387f8586c9c356687a623088ec450b
D-Shock <davidab028@gmail.com>
2025-11-28 10:50:02 -0600

    Primer programa con int, float y str

---
+++ Practice_1.0.py
@@ -0,0 +1,13 @@
+print("D-Shock was here")
+
+entero = 50
+decimal = 50.0
+nombre = "D-Shock;"
+
+print("Variable int:", entero)
+print("Variable float:", decimal)
+print("Variable str:", nombre)
+
+print("Suma de int + float:", entero + decimal)
+
+print("Hola,", nombre, "tu número favorito es", entero, "y su versión decimal es", decimal)
