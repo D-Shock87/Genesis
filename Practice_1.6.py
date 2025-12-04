29beafc7c70a189a8dfa1212b51a85e660422141
D-Shock <davidab028@gmail.com>
2025-12-04 13:38:27 -0600

    Comentarios añadidos al clasificador de números

--- Practice_1.6.py
+++ Practice_1.6.py
@@ -1,7 +1,10 @@
-#Prueba
+#Clasificador de números
 
+#Uso de la función input para variables float.
+#Uso del operador módulo (%) para determinar si un número es par o impar con base en su resultado.
 result = float(input("Please enter any number: ")) % 2
 
+#Uso de la estructura condicional if-else para indicar el número como par o impar.
 if result == 1:
     print("Odd")
 else:
