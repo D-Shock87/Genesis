37e72c275a667737098e1fa21331bf4f5585a978
D-Shock <davidab028@gmail.com>
2025-12-12 10:16:57 -0600

    Contador de números primos

---
+++ Practice_1.9.py
@@ -0,0 +1,15 @@
+#Contador de números primos
+#Primer uso del nested loop
+#Uso de f-string
+
+count_prime = 0
+for i in range(2, 3001):
+    prime = True
+    for j in range(2, i):
+        if i % j == 0 and j != 1 and j != i:
+            prime = False
+            break
+    if prime == True:
+        count_prime += 1
+
+print(f"En 3,000 números, existen {count_prime} que son primos.")
