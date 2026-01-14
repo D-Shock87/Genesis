1a10a5a0e3b845c7bd31dad64c68c09ae1f833df
D-Shock <davidab028@gmail.com>
2026-01-13 21:13:17 -0600

    FizzBuzz con comentarios

--- Practice_2.0.py
+++ Practice_2.0.py
@@ -1,5 +1,6 @@
 #FizzBuzz: Un clásico
 while True:
+#Utiliza un loop que solo se rompe por decisión del usuario
     user_input = (input("¡Bienvenido a FizzBuzz! Ingresa tu número o escribe Salir: "))
 
     try:
@@ -7,6 +8,9 @@ while True:
     except ValueError:
         print("¡Gracias por jugar!")
         break
+#Si el usuario escribe "Salir",
+#o realmente cualquier cosa que no sea un número,
+#el programa termina.
     if num % 3 == 0 and num % 7 == 0:
         print("¡Eso es un FizzBuzz!")
     elif num % 3 == 0:
